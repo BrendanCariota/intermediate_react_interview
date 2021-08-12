eb0ad07fe8539a3ef57948c84384b4bceff247c2
Brendan Cariota <brendan@cariota.com>
2021-08-12 02:51:35 -0500

    working on problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const BASE_URL = 'https://randomuser.me/api/?results=20'
+
+  const [dataHolder, setDataHolder] = useState('')
+
+  const fetchData = () => {
+    return axios.get(BASE_URL)
+      .then(({ data }) => {
+        console.log(data.results)
+        return data.results
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    const data = fetchData()
+    console.log(data)
+    setDataHolder(JSON.stringify(data))
+  }, [])
+
   return (
     <div className="App">
       <h1>Interview</h1>
+      <p>{dataHolder}</p>
     </div>
   );
 }
